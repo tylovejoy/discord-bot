8543a56227c9610b66a637957f489fa73de988f3
Ty Lovejoy <ty@fromskytocenter.com>
2021-08-31 22:52:01 -0500

    test

--- deploy-commands.js
+++ deploy-commands.js
@@ -14,49 +14,24 @@ const commands = [
 				.setRequired(true))
 		.addStringOption(option =>
 			option
-				.setName('map_name_1')
-				.setDescription('Name of the map (1 of 2)')
-				.setRequired(false)
-				.addChoice('Ayutthaya', 'ayutthaya')
-				.addChoice('Black Forest', 'blackforest')
-				.addChoice('Blizzard Wworld', 'blizzardworld')
-				.addChoice('Busan', 'busan')
-				.addChoice('Chateau Guillard', 'chateauguillard')
-				.addChoice('Dorado', 'dorado')
-				.addChoice('Ecopoint: Antarctica', 'ecopointantarctica')
-				.addChoice('Eichenwalde', 'eichenwalde')
-				.addChoice('Hanamura', 'hanamura')
-				.addChoice('Havana', 'havana')
-				.addChoice('Hollywood', 'hollywood')
-				.addChoice('Horizon Lunar Colony', 'horizonlunarcolony')
-				.addChoice('Ilios', 'ilios')
-				.addChoice('Junkertown', 'junkertown')
-				.addChoice('Kanezaka', 'kanezaka')
-				.addChoice('King\'s Row', 'kingsrow')
-				.addChoice('Lijiang Tower', 'lijiangtower')
-				.addChoice('Malevento', 'malevento')
-				.addChoice('Necropolis', 'necropolis')
-				.addChoice('Nepal', 'nepal')
-				.addChoice('Numbani', 'numbani')
-				.addChoice('Oasis', 'oasis')
-				.addChoice('Paris', 'paris')
-				.addChoice('Petra', 'petra')
-				.addChoice('Practice Range', 'practicerange'))
+				.setName('map')
+				.setDescription('Name of the map.')
+				.setRequired(true))
 		.addStringOption(option =>
 			option
-				.setName('map_name_2')
-				.setDescription('Name of the map (2 of 2)')
-				.setRequired(false)
-				.addChoice('Rialto', 'rialto')
-				.addChoice('Route 66', 'route66')
-				.addChoice('Temple of Anubis', 'templeofanubis')
-				.addChoice('Volskaya Industries', 'volskayaindustries')
-				.addChoice('Watchpoint Gibraltar', 'watchpointgibraltar')
-				.addChoice('Workshop Chamber', 'workshopchamber')
-				.addChoice('Workshop Expanse', 'workshopexpanse')
-				.addChoice('Workshop Greenscreen', 'workshopgreenscreen')
-				.addChoice('Workshop Island', 'workshopisland')
-				.addChoice('Framework', 'framework')),
+				.setName('maptype')
+				.setDescription('Type(s) of the map.')
+				.setRequired(true))
+		.addStringOption(option =>
+			option
+				.setName('creator')
+				.setDescription('Creator(s) of the map.')
+				.setRequired(true))
+		.addStringOption(option =>
+			option
+				.setName('description')
+				.setDescription('Description of the map.')
+				.setRequired(false)),
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
--- index.js
+++ index.js
@@ -7,4 +7,20 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'submitmap') {
+		const code = interaction.options.getString('code');
+		const map = interaction.options.getString('map');
+		const maptype = interaction.options.getString('maptype');
+		const creator = interaction.options.getString('creator');
+		const description = interaction.options.getString('description');
+		await interaction.reply({ content: `Code: ${code}, Map: ${map}, Maptype: ${maptype}, Creator: ${creator}, Description: ${description}`, ephemeral: true });
+	}
+});
+
 client.login(token);
